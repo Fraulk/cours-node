5833f381a2e2e4563251ccb919c005bf1608d145
Fraulk <fratulker@gmail.com>
2022-04-28 09:53:57 +0200

    detecte environnement dev

--- exo1/index.js
+++ exo1/index.js
@@ -2,3 +2,4 @@ console.log("hello world")
 const mavariable = process.env["mavariable"] || null
 if (mavariable == null) console.log("variable non defini, veuillez la definir")
 else console.log(mavariable)
+console.log(process.env["envContext"] == "dev" && "Je suis en dev" || process.env["envContext"] == "prod" && "Je suis en prod" || "pas d'environneement selectionné")
