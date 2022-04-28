261ae40dbc6e447bd285b3bf5f25af0883baa4ac
Fraulk <fratulker@gmail.com>
2022-04-28 09:49:18 +0200

    log mavariable

--- exo1/index.js
+++ exo1/index.js
@@ -1,1 +1,4 @@
 console.log("hello world")
+const mavariable = process.env["mavariable"] || null
+if (mavariable == null) console.log("variable non defini, veuillez la definir")
+else console.log(mavariable)
