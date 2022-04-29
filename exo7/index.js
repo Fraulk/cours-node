0f9df5562e978b96c1d454921c473089e1a0dbcf
f <fratulker@gmail.com>
2022-04-29 16:31:35 +0200

    ajout chronometre dans exo7

---
+++ exo7/index.js
@@ -0,0 +1,27 @@
+const http = require("http");
+
+const server = http.createServer((req, res) => {
+  try {
+    // ICI MIDDLEWARE A EXECUTER AVANT DE TRAITER LA REQUETE.
+    console.log(req.httpVersion, req.url, req.method);
+    console.time("timer name");
+    console.timeEnd("timer name");
+    // FIN MIDDLEWARE
+    if (req.url === "/") {
+      res.writeHead(200, { "content-type": "text/html" });
+      res.write("<h1>HOMEPAGE</h1>");
+      res.end();
+    } else {
+      res.writeHead(404, { "content-type": "text/html" });
+      res.write("<h1>404 Not Found</h1>");
+      res.end();
+    }
+  } catch (err) {
+    res.writeHead(500, { "content-type": "text/html" });
+    res.write("<h1>500 Internal Server Error</h1>");
+    res.end();
+  }
+});
+
+server.listen(3000);
+console.log("Server écoute sur le port 3000");
